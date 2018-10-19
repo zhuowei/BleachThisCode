803211d34ede9af0964a6741c47d459579c3ef04
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2018-10-19 14:19:54 -0700

    Add sample input and output

---
+++ samples/hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#define potato 1 + \
+               1
+
+int main(int argc, char** argv) {
+	/* prints Hello World */
+	printf("Hello world! %d %d\n", 12345, potato);
+	return 0;
+}
---
+++ samples/hello.out.c
@@ -0,0 +1,28 @@
+#define ​ 1
+#define ‌ +
+#define ‍ int
+#define ﻿ main
+#define ‌​ (
+#define ‌‌ argc
+#define ‌‍ ,
+#define ‌﻿ char
+#define ‍​ *
+#define ‍‌ argv
+#define ‍‍ )
+#define ‍﻿ {
+#define ﻿​ printf
+#define ﻿‌ "Hello world! %d %d\n"
+#define ﻿‍ 12345
+#define ﻿﻿ potato
+#define ‌​​ ;
+#define ‌​‌ return
+#define ‌​‍ 0
+#define ‌​﻿ }
+#include <stdio.h>
+#define potato ​ ‌ ​
+
+‍ ﻿ ‌​ ‍ ‌‌ ‌‍ ‌﻿ ‍​ ‍​ ‍‌ ‍‍ ‍﻿
+ 
+ ﻿​ ‌​ ﻿‌ ‌‍ ﻿‍ ‌‍ ﻿﻿ ‍‍ ‌​​
+ ‌​‌ ‌​‍ ‌​​
+‌​﻿
