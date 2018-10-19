3825ae8e40541396b367168b7a37261e6e7e4c6f
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2018-10-19 13:54:15 -0700

    Translate tokens and generate defines

--- main.cpp
+++ main.cpp
@@ -1,22 +1,99 @@
 #include <boost/wave.hpp>
 #include <boost/wave/cpplexer/cpp_lex_iterator.hpp>
 #include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <utility>
 typedef boost::wave::cpplexer::lex_token<> lex_token;
 typedef boost::wave::cpplexer::lex_iterator<lex_token> lex_iterator_type;
 using boost::wave::token_id;
+using namespace boost::wave;
+
+class TokenMapper {
+public:
+	std::vector<std::pair<std::string, std::string>> pairs;
+	std::unordered_map<std::string, std::string> tokenMap;
+	int curid = 0;
+
+	std::string mapToken(std::string const& inputToken);
+	void writeHeader(std::ofstream& output);
+	std::string translateToken(std::string const& inputToken);
+};
+
+std::string TokenMapper::mapToken(std::string const& inputToken) {
+	if (tokenMap.count(inputToken) == 0) {
+		std::string translatedToken = translateToken(inputToken);
+		tokenMap[inputToken] = translatedToken;
+		pairs.emplace_back(translatedToken, inputToken);
+	}
+	return tokenMap[inputToken];
+}
+
+std::string TokenMapper::translateToken(std::string const& inputToken) {
+	return "A" + std::to_string(curid++);
+}
+
+void TokenMapper::writeHeader(std::ofstream& output) {
+	for (auto const& pair: pairs) {
+		output << "#define " << pair.first << " " << pair.second << std::endl;
+	}
+}
+
 int main(int argc, char** argv) {
-	if (argc < 2) {
-		std::cerr << "usage: ./bleachthiscode <input.c>" << std::endl;
+	if (argc < 3) {
+		std::cerr << "usage: ./bleachthiscode <input.c> <output.c>" << std::endl;
 		return 1;
 	}
 	std::ifstream instream{argv[1]};
 	std::string instr{std::istreambuf_iterator<char>(instream.rdbuf()),
 		std::istreambuf_iterator<char>()};
+	std::ofstream fileoutput{argv[2], std::ios_base::out};
+	std::stringstream output;
 	lex_iterator_type iter(instr.begin(), instr.end(), {}, boost::wave::language_support::support_cpp);
 	lex_iterator_type iter_end = lex_iterator_type();
 	std::cout << "ok\n";
+	bool lastEmittedSpace = false;
+	bool lastTokenIsDefine = true;
+	TokenMapper mapper;
 	for (; iter != iter_end; iter++) {
 		lex_token const& token = *iter;
 		std::cout << token_id(token) << " " << boost::wave::get_token_name(token_id(token)) << " " << token.get_value() << std::endl;
+		if (IS_CATEGORY(token, KeywordTokenType) || IS_CATEGORY(token, StringLiteralTokenType) ||
+			IS_CATEGORY(token, OperatorTokenType) || IS_CATEGORY(token, IdentifierTokenType) || 
+			IS_CATEGORY(token, IntegerLiteralTokenType)) {
+			if (!lastEmittedSpace) {
+				// if the last emitted is not a space, manually emit a separator
+				output << " ";
+			}
+			if (!lastTokenIsDefine) {
+				output << mapper.mapToken(token.get_value().c_str());
+			} else {
+				// first arg of define is passed through unmodified.
+				output << token.get_value();
+			}
+			lastEmittedSpace = false;
+			lastTokenIsDefine = false;
+		} else if (token_id(token) == T_CCOMMENT || token_id(token) == T_CPPCOMMENT) {
+			// remove comments; we don't change the last emitted status
+		} else if (token_id(token) == T_NEWLINE) {
+			// newlines are printed directly
+			output << token.get_value();
+			lastEmittedSpace = true;
+		} else if (IS_CATEGORY(token, WhiteSpaceTokenType)) {
+			// other whitespace is turned into one space
+			output << " ";
+			lastEmittedSpace = true;
+		} else {
+			// pass through the rest
+			output << token.get_value();
+			lastEmittedSpace = false;
+			if (token_id(token) == T_PP_DEFINE) {
+				lastTokenIsDefine = true;
+			} else {
+				lastTokenIsDefine = false;
+			}
+		}
 	}
+	mapper.writeHeader(fileoutput);
+	fileoutput << output.str();
 }
