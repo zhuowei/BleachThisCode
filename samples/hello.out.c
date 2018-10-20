f17ff1965948d6adeb9908badfc1ef2ba354b5aa
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2018-10-19 20:45:58 -0700

    update sample with new output

--- samples/hello.out.c
+++ samples/hello.out.c
@@ -1,28 +1,25 @@
 #define ​ 1
 #define ‌ +
 #define ‍ int
-#define ﻿ main
-#define ‌​ (
-#define ‌‌ argc
-#define ‌‍ ,
-#define ‌﻿ char
-#define ‍​ *
-#define ‍‌ argv
-#define ‍‍ )
-#define ‍﻿ {
-#define ﻿​ printf
-#define ﻿‌ "Hello world! %d %d\n"
-#define ﻿‍ 12345
-#define ﻿﻿ potato
-#define ‌​​ ;
-#define ‌​‌ return
-#define ‌​‍ 0
-#define ‌​﻿ }
+#define ﻿ argc
+#define ‌​ char
+#define ‌‌ *
+#define ‌‍ argv
+#define ‌﻿ main( ‍ ﻿, ‌​ ‌‌ ‌‌ ‌‍)
+#define ‍​ {
+#define ‍‌ "Hello world! %d %d\n"
+#define ‍‍ 12345
+#define ‍﻿ potato
+#define ﻿​ printf( ‍‌, ‍‍, ‍﻿)
+#define ﻿‌ ;
+#define ﻿‍ return
+#define ﻿﻿ 0
+#define ‌​​ }
 #include <stdio.h>
 #define potato ​ ‌ ​
 
-‍ ﻿ ‌​ ‍ ‌‌ ‌‍ ‌﻿ ‍​ ‍​ ‍‌ ‍‍ ‍﻿
+‍  ‌﻿ ‍​
  
- ﻿​ ‌​ ﻿‌ ‌‍ ﻿‍ ‌‍ ﻿﻿ ‍‍ ‌​​
- ‌​‌ ‌​‍ ‌​​
-‌​﻿
+  ﻿​ ﻿‌
+ ﻿‍ ﻿﻿ ﻿‌
+‌​​
